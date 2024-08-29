132f4be27df3a2f67526d76cd454b5ac2f942b4e
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-08-29 14:51:27 +0700

    fix

--- src/ThongTinVatPham/VatPham.java
+++ src/ThongTinVatPham/VatPham.java
@@ -1,7 +1,7 @@
 package ThongTinVatPham;
 
 public class VatPham {
-    //Instance Fields
+    // Instance Fields
     protected int chiSoSucManh;
     protected float heSoSucManh;
     protected int soVang;
@@ -14,32 +14,31 @@ public class VatPham {
     public float tongSoVangQuyDoi() {
         return this.tinhSucCongPha() * soVang;
     }
-    
+
     public void xuat() {
         System.out.println("----------------");
-        System.out.println("-- Ten: " + checkVatPham());
-        System.out.println("-- Chi so suc manh: " + chiSoSucManh);
-        System.out.println(" -- He so suc manh: " + heSoSucManh);
-        System.out.println("-- Suc cong pha: " + tinhSucCongPha());
-        System.out.println("-- So vang (quy doi/1 suc cong pha): " + tongSoVangQuyDoi());
+        System.out.println("-- Ten: " + checkVatPham() + "-- Chi so suc manh: " + chiSoSucManh + " -- He so suc manh: "
+                + heSoSucManh + "-- Suc cong pha: " + tinhSucCongPha() + "-- So vang (quy doi/1 suc cong pha): "
+                + tongSoVangQuyDoi());
+        // System.out.println("-- Chi so suc manh: " + chiSoSucManh);
+        // System.out.println(" -- He so suc manh: " + heSoSucManh);
+        // System.out.println("-- Suc cong pha: " + tinhSucCongPha());
+        // System.out.println("-- So vang (quy doi/1 suc cong pha): " +
+        // tongSoVangQuyDoi());
     }
+
     private String checkVatPham() {
-        if(this instanceof ChiecBuaThan) {
+        if (this instanceof ChiecBuaThan) {
             return "Chiec bua than";
-        }
-        else if(this instanceof BaoTaySamSet) {
+        } else if (this instanceof BaoTaySamSet) {
             return "Bao tay sam set";
-        }
-        else if(this instanceof HonDaKichNo) {
+        } else if (this instanceof HonDaKichNo) {
             return "Hon da kich no";
-        }
-        else if(this instanceof CayRiuPhepThuat) {
+        } else if (this instanceof CayRiuPhepThuat) {
             return "Cay riu phep thuat";
-        }
-        else if(this instanceof CayGayChienThan) {
+        } else if (this instanceof CayGayChienThan) {
             return "Cay gay chien than";
-        }
-        else{
+        } else {
             return "";
         }
     }
